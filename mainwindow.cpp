e3142f0bee908bf858cab42cdd95680bb92dcfbe
阿宝 <talent518@yeah.net>
2021-07-18 07:08:41 +0800

    完善并美化界面绘图，修复ubuntu平台下编译问题。

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,15 +6,19 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    secondTimer=new QTimer;
-    secondTimer->start(1000);
+    resize(3*WS_PX + (WIDTH_SHAPE_NUM+4)*SIDE_LEN, 2*WS_PX + HEIGHT_SHAPE_NUM*SIDE_LEN);
+    secondTimer=new QTimer(this);
+    secondTimer->start(50);
     connect(secondTimer,SIGNAL(timeout()),SLOT(secondTimerEvent()));
-    mainTimer=new QTimer;
+    secondTimer->stop();
+    mainTimer=new QTimer(this);
     mainTimer->start(1000);
     connect(mainTimer,SIGNAL(timeout()),SLOT(mainTimerEvent()));
     mainTimer->stop();
-    qsrand(time(0));
+    qsrand(clock());
+    nextColor = randColor();
     initGame();
+    overStep = -10;
 }
 
 MainWindow::~MainWindow()
@@ -22,8 +26,6 @@ MainWindow::~MainWindow()
     delete ui;
     delete mainTimer;
     delete secondTimer;
-    delete squareRecords;
-    delete colorRecords;
 }
 
 void MainWindow::changeEvent(QEvent *e)
@@ -38,41 +40,121 @@ void MainWindow::changeEvent(QEvent *e)
     }
 }
 
+void drawRect(QPainter& painter, QRect& rect, QColor& color) {
+    painter.fillRect(rect, color);
+
+    painter.setPen(QColor(color.red()+0x33, color.green()+0x33, color.blue()+0x33));
+    painter.drawLine(rect.topLeft(), rect.topRight()); // 上连线
+    painter.drawLine(rect.topLeft(), rect.bottomLeft()); // 左边线
+
+    painter.setPen(QColor(color.red()-0x33, color.green()-0x33, color.blue()-0x33));
+    painter.drawLine(rect.topRight(), rect.bottomRight()); // 右边线
+    painter.drawLine(rect.bottomLeft(), rect.bottomRight()); // 下边线
+}
+
 void MainWindow::paintEvent(QPaintEvent *e){
     QPainter painter(this);
     int x,y;
-    painter.setPen(QColor(102,102,102));
-    painter.drawRect(20,20,SIDE_LEN*WIDTH_SHAPE_NUM,SIDE_LEN*HEIGHT_SHAPE_NUM);
-    painter.drawRect(40+WIDTH_SHAPE_NUM*SIDE_LEN,20,SIDE_LEN*4,SIDE_LEN*4);
+    QRect mainRect(WS_PX-1,WS_PX-1,SIDE_LEN*WIDTH_SHAPE_NUM+1,SIDE_LEN*HEIGHT_SHAPE_NUM+1);
+    QRect nextRect(2*WS_PX+WIDTH_SHAPE_NUM*SIDE_LEN-1,WS_PX-1,SIDE_LEN*4+1,SIDE_LEN*4+1);
+    painter.fillRect(0, 0, width(), height(), QColor(255,255,255));
+    painter.setPen(QColor(0x33,0x33,0x33));
+    painter.drawRect(mainRect); // 主游戏窗
+    painter.drawRect(nextRect); // 下一个形状窗
 
-    painter.drawText(QPoint(40+WIDTH_SHAPE_NUM*SIDE_LEN,40+SIDE_LEN*4),QString("Scores:%1,Lines:%2").arg(scoreNum).arg(lineNum));
+    painter.setFont(QFont(QString(""), 16, 100, false));
 
+    y = WS_PX + painter.fontInfo().pixelSize() / 2;
+    {
+        QString strs[] = {QString("分数：%1").arg(scoreNum), QString("行数：%1").arg(lineNum)};
+        QPointF p(nextRect.left(), nextRect.bottom() + WS_PX);
+
+        painter.setPen(curColor);
+        for(int i=0; i<sizeof(strs)/sizeof(QString); i++) {
+            p.setY(p.y() + y);
+            painter.drawText(p, strs[i]);
+        }
+    }
+
+    y = WS_PX + painter.fontInfo().pixelSize() / 2;
+    {
+        QString strs[] = {
+            QString("变形：⇈"),
+            QString("左移：⇇"),
+            QString("右移：⇉"),
+            QString("下落：⇊"),
+            QString("落下：空格"),
+            QString("新建：F6"),
+            QString("暂停：F5"),
+        };
+        QPointF p(nextRect.left(), mainRect.bottom() + WS_PX);
+
+        painter.setPen(QColor(0xff,0x22,0x00));
+        for(int i=sizeof(strs)/sizeof(QString)-1; i>=0; i--) {
+            p.setY(p.y() - y);
+            painter.drawText(p, strs[i]);
+        }
+    }
+
+    {
+        QPointF p(nextRect.left() + nextRect.width()/2, mainRect.top() + mainRect.height()/2);
+
+        painter.setPen(Qt::red);
+        painter.setFont(QFont(QString(""), 80, 100, false));
+        p.setX(p.x() - painter.fontInfo().pixelSize() / 2);
+        p.setY(p.y() + painter.fontInfo().pixelSize() / 2);
+        painter.drawText(p, QString("囍"));
+    }
+
+    // 下一个形状
     for(y=0;y<4;y++)
         for(x=0;x<4;x++){
             if(x+sX>=0 && y+sY>=0 && x+sX<WIDTH_SHAPE_NUM && y+sY<HEIGHT_SHAPE_NUM && getPointB(curShape,x,y)){
-                painter.setBrush(curColor);
-                painter.drawRect(20+(x+sX)*SIDE_LEN,20+(y+sY)*SIDE_LEN,SIDE_LEN,SIDE_LEN);
+                QRect rect(WS_PX+(x+sX)*SIDE_LEN,WS_PX+(y+sY)*SIDE_LEN,SIDE_LEN,SIDE_LEN);
+                ::drawRect(painter, rect, curColor);
             }
             if(getPointB(nextShape,x,y)){
-                painter.setBrush(nextColor);
-                painter.drawRect(40+(x+WIDTH_SHAPE_NUM)*SIDE_LEN,20+y*SIDE_LEN,SIDE_LEN,SIDE_LEN);
+                QRect rect(2*WS_PX+(x+WIDTH_SHAPE_NUM)*SIDE_LEN,WS_PX+y*SIDE_LEN,SIDE_LEN,SIDE_LEN);
+                ::drawRect(painter, rect, nextColor);
             }
         }
+
+    // 主游戏窗
     for(y=0;y<HEIGHT_SHAPE_NUM;y++)
         for(x=0;x<WIDTH_SHAPE_NUM;x++)
             if(squareRecords[x][y]){
-                painter.setBrush(colorRecords[x][y]);
-                painter.drawRect(20+x*SIDE_LEN,20+y*SIDE_LEN,SIDE_LEN,SIDE_LEN);
+                QRect rect(WS_PX+x*SIDE_LEN,WS_PX+y*SIDE_LEN,SIDE_LEN,SIDE_LEN);
+                ::drawRect(painter, rect, colorRecords[x][y]);
             }
+
+    // 游戏结束
     if(endGame){
-        painter.setPen(Qt::red);
-        painter.setFont(QFont(QString(""),20,100));
-        painter.drawText(QPoint(60,200),QString("Game Over!"));
+        QString str("游戏结束！");
+        QRect rect(painter.boundingRect(mainRect, Qt::AlignCenter, str));
+
+        int alpha = overColor.alpha() + overStep;
+        if(overStep > 0) {
+            if(alpha > 0xff) {
+                alpha = 0xff;
+                overStep = -overStep;
+            }
+        } else {
+            if(alpha < 0) {
+                alpha = 0;
+                overStep = -overStep;
+                overColor = randColor();
+            }
+        }
+        overColor.setAlpha(alpha);
+        painter.setPen(overColor);
+        painter.setFont(QFont(QString(""), 40, 100, false));
+        painter.drawText(mainRect, Qt::AlignCenter, str, &rect);
     }
 }
 
 void MainWindow::secondTimerEvent()
 {
+    if(endGame) repaint();
 }
 void MainWindow::mainTimerEvent()
 {
@@ -114,11 +196,11 @@ void MainWindow::keyPressEvent(QKeyEvent *e){
 }
 
 bool MainWindow::getPointB(int p,int x,int y){
- return (p&(1<<(16-(x+1+y*4))));
+    return (p&(1<<(16-(x+1+y*4))));
 }
 
 void MainWindow::moveShape(Move m){
-    if(!mainTimer->isActive() || endGame) return;
+    if(!mainTimer->isActive() || endGame) {return;}
     switch(m){
     case(Up):
         if(ifMovable(rotateShape(curShape),sX,sY))
@@ -155,7 +237,9 @@ void MainWindow::setNextShape(){
     if(!ifMovable(nextShape,sX,sY)){
         curShape=0;
         mainTimer->stop();
+        secondTimer->start();
         endGame=true;
+        overColor = randColor();
         repaint();
     }
     curShape=nextShape;
@@ -182,6 +266,7 @@ bool MainWindow::ifMovable(int shape,int X,int Y){
     return true;
 }
 void MainWindow::initGame(){
+    secondTimer->stop();
     endGame=false;
     curShape=0;
     nextShape=SHAPES[qrand()%SHAPE_NUM];
@@ -197,19 +282,6 @@ QColor MainWindow::randColor(){
     r=qrand()%COLOR_NUM;
     g=qrand()%COLOR_NUM;
     b=qrand()%COLOR_NUM;
-    while(r==g && r==b){
-        switch(qrand()%3){
-        case(0):
-            r=qrand()%COLOR_NUM;
-            break;
-        case(1):
-            g=qrand()%COLOR_NUM;
-            break;
-        case(2):
-            b=qrand()%COLOR_NUM;
-            break;
-        }
-    }
     return QColor(COLORS[r],COLORS[g],COLORS[b]);
 }
 void MainWindow::setRecord(){
